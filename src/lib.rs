3f1e5be787264be73fbf2b5a9f69090812c4731d
VietThanLinux <thanhoangviet@gmail.com>
2023-04-26 20:51:53 -0400

    testing with random ports

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -13,7 +14,7 @@ async fn health_check() -> impl Responder {
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     // HttpServer::new(|| {
     //     App::new()
     //         .route("/", web::get().to(greet))
@@ -27,7 +28,7 @@ pub fn run() -> Result<Server, std::io::Error> {
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
